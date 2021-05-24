ad0d2efe794daff6eb037f9efb96cfb44981933f
stephje <s.jenkins3018@gmail.com>
2021-05-24 13:39:12 +0800

    Created seed SQL

---
+++ db/seed.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+USE employeeDB;
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(100) NOT NULL,
+  salary DECIMAL(8, 2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments(id),
+  PRIMARY KEY (id)
+);
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  role_id INT NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  manager_id INT NOT NULL,
+  FOREIGN KEY (manager_id) REFERENCES employees(id),
+  PRIMARY KEY (id)
+);
